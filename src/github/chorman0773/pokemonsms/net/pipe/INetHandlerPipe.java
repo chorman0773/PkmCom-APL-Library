349ecc9221600a3942d29094ffb0b1a72fdd8ec5
Connor Horman <chorman64@gmail.com>
2019-05-24 10:54:49 -0400

    Add Documentation for INetHandlerPipe

--- src/github/chorman0773/pokemonsms/net/pipe/INetHandlerPipe.java
+++ src/github/chorman0773/pokemonsms/net/pipe/INetHandlerPipe.java
@@ -4,6 +4,10 @@ import github.chorman0773.pokemonsms.net.INetHandlerRemote;
 import github.chorman0773.pokemonsms.net.IPacket;
 import github.chorman0773.pokemonsms.net.ProtocolError;
 
+/**
+* An INetHandlerPipe is a Connection to an internal INetHandlerRemote via an internal pipe.
+* Packets that are sent across pipes are not serialized, and simply queued at the other end.
+*/
 public interface INetHandlerPipe extends INetHandlerRemote {
 	INetHandlerPipe getOtherSide();
 	void enqueuePacket(IPacket packet) throws ProtocolError;
